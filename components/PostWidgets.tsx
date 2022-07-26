bcd9151fe24628384b470639441cab348bc134f4
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-26 18:10:07 +0100

    Adding the components

---
+++ components/PostWidgets.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostWidgets = () => {
+  return (
+    <div>PostWidgets</div>
+  )
+}
+
+export default PostWidgets
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,23 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="conatiner mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-600">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className='lg:col-span-8 col-span-1'>
         {posts.map((post,index) => (
           <div>
             {post.title}
             {post.excerpt}
           </div>
         ))}
+        </div>
+          <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'></div>
+          </div>
       </div>
     </div>
   )
