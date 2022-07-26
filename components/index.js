3819187aaac25d98dae6ae1f3531848e4c2542b0
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-26 18:54:50 +0100

    Added the components and passed demo data

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export {default as PostCard} from './PostCard'
+export {default as PostWidgets} from './PostWidgets'
+export {default as Categories} from './Categories'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import {PostCard, PostWidgets,Categories} from '../components'
 
 const posts = [
   {title: 'React Testing', excerpt: 'Learn React Testing'},
@@ -11,20 +11,20 @@ const Home: NextPage = () => {
   return (
     <div className="container mx-auto px-10 mb-8 bg-gray-600">
       <Head>
-        <title>Create Next App</title>
+        <title>CMS BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
         {posts.map((post,index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
+          <PostCard post={post} key={post.title} />
         ))}
         </div>
           <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'></div>
+            <div className='lg:sticky relative top-8'>
+              <PostWidgets/>
+              <Categories/>
+            </div>
           </div>
       </div>
     </div>
