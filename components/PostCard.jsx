1d47531668367c0bb66bbca9b2c99250b8d4fdb1
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-29 19:51:14 +0100

    Added the name and image to the postcard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,6 +9,17 @@ const PostCard = ({post}) => {
        <div className="relative overflow-hidden shadow-md pb-80 mb-6">
         <img src={post.featureImage.url} alt={post.title} className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" />
        </div>
+       <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'
+       >
+        <Link href={`/post/${post.slug}`}>
+          {post.title}
+        </Link>
+       </h1>
+       <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
+        <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
+          <img alt={post.author.name} height='30px' width='30px' className='align-middle rounded-full' src={post.author.photo.url} />
+        </div>
+       </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import {getPosts} from '../services'
 
 export default function Home({posts}) {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-600">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS BLOG</title>
         <link rel="icon" href="/favicon.ico" />
