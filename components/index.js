32404a3dada3ab4449d63399b989c9ac1a08d085
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-29 10:42:50 +0100

    Add a demo category, and map through it

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export {default as PostCard} from './PostCard'
 export {default as PostWidgets} from './PostWidgets'
 export {default as Categories} from './Categories'
+export {default as Header} from './Header'
