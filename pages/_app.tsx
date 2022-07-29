abac0596a2273c6db7efd5f4f3bf86dfaf9a6128
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-29 10:58:14 +0100

    Created a layout component that displays the header on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
+import React, {useEffect,useState} from 'react'
+import { Layout } from '../components'
+
 import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+  <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp
