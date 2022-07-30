d32098d8f3674a92c60e798ea9b114f7e5153550
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-31 00:21:48 +0100

    Add logic to display posts when slug is true or false

--- components/PostWidgets.jsx
+++ components/PostWidgets.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
-const PostWidgets = () => {
+
+const PostWidgets = ({categories,slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+  useEffect(() => {
+   if(slug) {
+    getSimilarPosts(categories, slug)
+    .then((result) => setRelatedPosts( result))
+   } else {
+    getRecentPosts()
+    .then((result) => setRelatedPosts( result))
+   }
+  }, [slug])
+  console.log(relatedPosts);
   return (
-    <div>PostWidgets</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+    </div>
   )
 }
 
--- services/index.js
+++ services/index.js
@@ -36,3 +36,43 @@ export const getPosts = async () => {
     const results = await request(graphqlAPI,query)
     return results.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetPostDetails() {
+        posts(
+            orderBy: createdAt_ASC
+            last: 3
+            ) {
+                title
+                featureImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+    }
+    `
+    const results = await request(graphqlAPI,query)
+    return results.posts;
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql `
+    query GetPostDetails($slug: String!, $categories: [String]) {
+        posts (
+            where: {slug_not: $slug AND: {categories_some: {slug_in: $categories}}}
+            last: 3
+        ){
+            title
+            featureImage {
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+    const results = await request(graphqlAPI,query)
+    return results.posts;
+}
