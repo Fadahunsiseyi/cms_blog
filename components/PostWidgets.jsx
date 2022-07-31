a6f6bd8d998efb4aeb9c35bbede4fa1e17df151d
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-07-31 22:19:24 +0100

    Added the texts and images to the widget component from gql

--- components/PostWidgets.jsx
+++ components/PostWidgets.jsx
@@ -18,9 +18,22 @@ const PostWidgets = ({categories,slug}) => {
   console.log(relatedPosts);
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
-      <h3>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
         {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className='flex items-center w-full'>
+          <div className='w-16 mb-3 flex-none'>
+            <img alt={post.title} height='60px' width='60px' className='align-middle rounded-full' src={post.featureImage.url} />
+          </div>
+          <div className='flex-grow ml-4'>
+            <p className='text-gray-500 font-xs'>{moment(post.createdAt).format('DD.MMM.YYYY')}</p>
+            <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
