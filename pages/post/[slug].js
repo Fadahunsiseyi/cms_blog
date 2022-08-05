ad764f6dde49747f49ca28c2838ba16e31ddf73c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-05 12:18:38 +0100

    Added featuredpost to the index page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {useRouter} from 'next/router'
 import {getPosts, GetPostDetails} from '../../services'
-import {PostDetail, Categories, PostWidgets, Author, Comments, CommentsForm} from '../../components'
+import {PostDetail, Categories, PostWidgets, Author, Comments, CommentsForm,Loader} from '../../components'
 
 const PostDetails = ({post}) => {
+  const router = useRouter()
+  if(router.isFallback) {
+    return <Loader/>
+  }
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
@@ -40,6 +45,6 @@ export async function getStaticProps({params}) {
 
     return {
         paths: posts.map(({node: {slug}}) => ({params: {slug}})),
-        fallback: false
+        fallback: true
     }
   }
